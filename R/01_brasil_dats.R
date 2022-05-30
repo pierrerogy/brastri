7816df2712b40259b2063d8780a31db76551c095
“pierrerogy” <>
2022-05-30 14:05:23 -0700

    done with data cleaning and preparation for invertebrates

--- R/01_brasil_dats.R
+++ R/01_brasil_dats.R
@@ -1,11 +1,12 @@
-# Cleaning and exploring data from Brazil
+# Analysis of Brasil data
 
 # Libraries
 library(tidyverse)
 library(ggplot2)
 library(here)
+library(hellometry)
 
-# Read data
+# Load data
 bichos <- 
   readr::read_csv(here::here("brasil",
                              "bichos.csv"))
@@ -16,6 +17,21 @@ communities <-
   readr::read_csv(here::here("brasil",
                              "communities.csv"))
 
+# Prepare data on bromeliads and decomposition ---------------------------------------------
+# Get list of bromeliads and treatments
+bromeliad_stub <- 
+  bromeliads %>% 
+  ## Only keep experimental ones
+  dplyr::filter(stringr::str_detect(bromeliad_id,
+                                    "E")) %>% 
+  ## Get percent difference in mass of leaf_litter
+  dplyr::mutate(coarse_dec = (coarse_init_g - coarse_final_dry_g)/coarse_init_g,
+                fine_dec = (fine_init_g - fine_final_dry_g)/fine_init_g) %>%
+  ## Only keep relevant columns
+  dplyr::select(-day, -coarse_det_g, -medium_det_g,
+                -fine_det_g, -coarse_final_g, -fine_final_g,
+                -coarse_init_g, -coarse_final_dry_g,
+                -fine_init_g, -fine_final_dry_g)  
 
 # Add missing bits of taxonomy --------------------------------------------
 bichos <- 
@@ -51,12 +67,12 @@ bichos <-
                                           "Forcipomyiinae",
                                           ifelse(Family == "Ceratopogonidae" & 
                                                    Species %in% c("sp4","sp6", "sp7"),
-                                                 "Leptoconopinae", Subfamily))))
-  
+                                                 "Leptoconopinae", Subfamily)))) %>% 
+  ## Tipulid genus
+  dplyr::mutate(Genus = ifelse(Family == "Tipulidae",
+                               "Trentepohlia", Genus))
 
-
-
-# Filtering relevant insects from the emergence traps ---------------------
+# Preparing emergence data ---------------------
 # Data
 bichos_emerged <- 
   bichos %>% 
@@ -76,73 +92,182 @@ bichos_emerged <-
   dplyr::mutate(date = lubridate::dmy(date)) %>% 
   ## Convert to day number and remove day number of first day
   dplyr::mutate(days_emergence = lubridate::yday(date) - 
-                  lubridate::yday(lubridate::as_date(lubridate::dmy("31/03/2022"))))
-
-# See how many we have in terms of families
-bichos_final <- 
-  bichos_emerged %>% 
-  dplyr::select(Class, Order, Family, Subfamily, Genus, n) %>% 
-  dplyr::group_by(Class, Order, Family, Subfamily, Genus) %>% 
-  dplyr::summarise_all(sum) %>% 
-  dplyr::ungroup()
-
-
-# Summarising communities -------------------------------------------------
-# Data
-communities_end <- 
-  communities %>% 
-  ## Data from after the manipuation
-  dplyr::filter(date == "04/05/2022")
-
-# Sum at subfamilylevel
-communities_final <- 
-  communities_end %>% 
-  ## remove unidentified debris
-  dplyr::filter(!is.na(Class)) %>% 
-  dplyr::select(Class, Order, Family, Subfamily, Genus) %>% 
-  dplyr::group_by(Class, Order, Family, Subfamily, Genus) %>% 
-  dplyr::tally() %>% 
-  dplyr::ungroup()
-
-
-# How many did we end up with ---------------------------------------------
-final <- 
-  communities_final %>%
-  dplyr::rename(n_in_water = n) %>%
-  ## Attach emerged
-  dplyr::left_join(bichos_final %>% 
-                     dplyr::rename(n_emerged = n),
-                   by = c("Class", "Order", 
-                          "Family", "Subfamily", "Genus")) %>% 
-  ## Make 0 for NAs in emerged
-  dplyr::mutate(n_emerged = ifelse(is.na(n_emerged),
-                                   0, n_emerged)) %>% 
-  ## Add row for Polypedilum
-  tibble::add_row(Class = "Hexapoda",
-                  Order = "Diptera",
-                  Family = "Chironomidae",
-                  Subfamily = "Chironominae",
-                  Genus = "Polypedilum",
-                  n_in_water = NA,
-                  n_emerged = NA) %>% 
-  ## Make column with quantity of larvae in input 
-  dplyr::mutate(input = ifelse(Family == "Culicidae",
-                               7 * 32, ifelse(
-                                 Family == "Coenagrionidae",
-                                 1 * 16, ifelse(
-                                   Family == "Tipulidae",
-                                   3 * 16, ifelse(
-                                     Subfamily == "Chironominae" & Genus == "Polypedilum",
-                                                  4 * 32, ifelse(
-                                                    Subfamily == "Tanypodinae",
-                                                    1 * 32, NA)))))) %>% 
-  ## Now get how many we salvaged at the end
-  dplyr::mutate(prop_salvaged = (n_in_water + n_emerged)/input) %>% 
-  ## Add 0 to the others
-  dplyr::mutate(input = ifelse(is.na(input),
-                               0, input)) 
+                  lubridate::yday(lubridate::as_date(lubridate::dmy("31/03/2022")))) %>% 
+  ## Remove biomass of mosquito full of blood to replace it with estimation
+  dplyr::mutate(dry_mass_mg = ifelse(stringr::str_detect(notes,
+                                                         "blood"),
+                                     NA, dry_mass_mg)) %>% 
+  ## Remove the notes and empty columns
+  dplyr::select(-notes, -'...13', -'...14', -'...15')
   
-## Save final data
-readr::write_csv(final,
-                 here::here("brasil",
-                            "final.csv"))
+# Estimation of biomass using hellometry
+bichos_biomass_est <- 
+  bichos_emerged %>% 
+  ## Rename columns
+  dplyr::rename(size_mm = length_mm,
+                abundance = n) %>% 
+  ## Add columns needed by package
+  dplyr::mutate(stage ="adult",
+                bwg_name = NA,
+                biomass_type = "dry") %>% 
+  ## Add unknown to missing sizes
+  dplyr::mutate(size_mm = ifelse(size_mm == "missing" |
+                                   is.na(size_mm),
+                                 "unknown", size_mm)) %>% 
+  ## Rename column with biomass
+  dplyr::rename(biomass_mg = dry_mass_mg) %>% 
+  ## Add taxonomy
+  hellometry::add_taxonomy() %>% 
+  ## Remove columns we have already, then rename old ones
+  dplyr::mutate(class = Class,
+                ord = Order,
+                family = Family,
+                subfamily = Subfamily,
+                genus = Genus,
+                species = Species) %>% 
+  dplyr::select(-Order, -Family, -Subfamily, -Genus, -Class, -Species) %>% 
+  ## Get estimations
+  hellometry::hello_metry()
+
+# Trim data back into nice format
+emergence <- 
+  bichos_biomass_est %>% 
+  ## Make column with taxon name, first 4 letters of string
+  dplyr::mutate(taxon = ifelse(is.na(subfamily),
+                               substr(genus,1,4), 
+                               substr(subfamily,1,4))) %>% 
+  ## Last NA left is Odonate
+  dplyr::mutate(taxon = ifelse(is.na(taxon),
+                               "Odon", taxon)) %>% 
+  dplyr::select(taxon , species, bromeliad_id, date, 
+                size_original, days_emergence,
+                abundance, biomass_mg) %>% 
+  ## Keep relevant columns
+  dplyr::select(bromeliad_id, taxon, abundance, 
+                biomass_mg, days_emergence) %>% 
+  ## Make biomass numeric
+  dplyr::mutate(biomass_mg = as.numeric(biomass_mg)) %>% 
+  ## Add treatments
+  dplyr::left_join(bromeliad_stub %>% 
+                     dplyr::select(bromeliad_id, predator,
+                                   resource),
+                   by = "bromeliad_id")
+  
+
+
+# Preparing leftover communities -------------------------------------------------------------------
+leftover <- 
+  communities %>% 
+  ## Keep those after the manipuation
+  dplyr::filter(date == "04/05/2022") %>% 
+  ## Remove extra column at end and date
+  dplyr::select(-date, -'...11') %>% 
+  ## Remove destroyed, unidentifiable specimen
+  dplyr::filter(!is.na(Class)) %>% 
+  ## Rename column
+  dplyr::rename(size_mm = length_mm) %>% 
+  ## Add columns needed by package
+  dplyr::mutate(stage = ifelse(Species == "pupa",
+                               "pupa", "larva"),
+                bwg_name = NA,
+                abundance = 1,
+                biomass_type = "dry") %>% 
+  ## Add unknown to missing sizes
+  dplyr::mutate(size_mm = ifelse(size_mm == "missing" |
+                                   is.na(size_mm),
+                                 "unknown", size_mm)) %>% 
+  ## Rename column with biomass
+  dplyr::rename(biomass_mg = dry_mass_mg) %>% 
+  ## Add taxonomy
+  hellometry::add_taxonomy() %>% 
+  ## Add subclass for leeches
+  dplyr::mutate(subclass = ifelse(stringr::str_detect(Species, "leech"),
+                                  "Hirudinea", subclass)) %>% 
+  ## Remove columns we have already, then rename old ones
+  dplyr::mutate(class = Class,
+                ord = Order,
+                family = Family,
+                subfamily = Subfamily,
+                genus = Genus,
+                species = Species) %>% 
+  dplyr::select(-Order, -Family, -Subfamily, -Genus, -Class, -Species) %>% 
+  ## Get estimations
+  hellometry::hello_metry() %>% 
+  ## Make column with taxon name, first 4 letters of string
+  dplyr::mutate(taxon = ifelse(!is.na(genus),
+                               substr(genus,1,4), 
+                               ifelse(!is.na(subfamily),
+                                      substr(subfamily,1,4), 
+                                      ifelse(!is.na(family),
+                                             substr(family,1,4),
+                                             ifelse(!is.na(ord),
+                                                    substr(ord,1,4),
+                                                    substr(class,1,4)))))) %>% 
+  ## Keep relevant columns
+  dplyr::select(bromeliad_id, taxon, abundance, 
+                biomass_mg) %>% 
+  ## Make biomass numeric
+  dplyr::mutate(biomass_mg = as.numeric(biomass_mg)) %>% 
+  ## Add treatments
+  dplyr::left_join(bromeliad_stub %>% 
+                     dplyr::select(bromeliad_id, predator,
+                                   resource),
+                   by = "bromeliad_id")
+
+
+
+# Gather how many we were able to salvage ---------------------------------------------
+salvaged <- 
+  # Create blank tibble with the number of input larvae
+  tibble::tibble(bromeliad_id = rep(bromeliad_stub$bromeliad_id,
+                                    each = 5),
+                 predator = rep(bromeliad_stub$predator,
+                                each = 5),
+                 resource = rep(bromeliad_stub$resource,
+                                each = 5),
+          taxon = rep(c("Tany", "Poly", 
+                       "Weom", "Tipu",
+                       "Odon"), 32),
+          n_input = c(rep(c(1, 4, 7, 3, 1), 16),
+                    rep(c(1, 4, 7, 3, 0), 16))) %>% 
+  # Add emergence, sum by bromeliad only for relevant taxa
+  dplyr::left_join(emergence %>%
+                     dplyr::select(-biomass_mg, -days_emergence, 
+                                   -abundance) %>% 
+                     dplyr::filter(taxon %in% c("Tany", "Poly", 
+                                                "Weom", "Tipu",
+                                                "Odon")) %>% 
+                     dplyr::group_by(dplyr::across()) %>% 
+                     dplyr::tally() %>% 
+                     dplyr::rename(n_emerged = n),
+                   by = c("bromeliad_id", "taxon","predator", "resource")) %>% 
+  # Add emergence, sum by bromeliad only for relevant taxa, also add treatments
+  dplyr::left_join(leftover %>% 
+                     dplyr::select(-biomass_mg, -abundance) %>% 
+                     dplyr::filter(taxon %in% c("Tany", "Poly", 
+                                                "Weom", "Tipu",
+                                                "Odon")) %>% 
+                     dplyr::group_by(dplyr::across()) %>% 
+                     dplyr::tally() %>% 
+                     dplyr::rename(n_leftover = n),
+                   by = c("bromeliad_id", "taxon", "predator", "resource")) %>% 
+  # Replace NAs by 0s in the joined columns
+  tidyr::replace_na(list(n_emerged = 0,
+                         n_leftover = 0)) %>% 
+  # Do the sum of how much we gathered
+  dplyr::mutate(prop_emerged = n_emerged/n_input,
+                prop_leftver = n_leftover/n_input,
+                prop_salvaged = (n_emerged + n_leftover)/n_input)
+  
+# models ------------------------------------------------------------------
+
+
+  
+
+# Model on survival rates
+
+# Models on water parameters
+
+# Models on biomass out
+
+# Models on extra stuff in
